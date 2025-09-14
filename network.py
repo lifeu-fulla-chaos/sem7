6cbd0ede75fd047d7b5d6a69fd9636af1734883a
mbuyt0n <shusrith03@gmail.com>
2025-09-14 16:49:48 +0530

    how was this even working

--- network.py
+++ network.py
@@ -67,7 +67,7 @@ class NetworkManager:
                     return line
         elif self.mode == "udp":
             while True:
-                data = self.socket.recv(16391)  # type: ignore
+                data, _ = self.socket.recvfrom(16391)  # type: ignore
                 print("data secvd", len(data))
                 return data
 
