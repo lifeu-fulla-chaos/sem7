03da332f19f97093cfb62a6b892bca2fbd784fbc
mbuyt0n <shusrith03@gmail.com>
2025-09-15 08:10:10 +0530

    i got audio but i got shit audio

--- audio.py
+++ audio.py
@@ -66,9 +66,8 @@ class AudioHandler:
                 header = data[:6]
                 iteration = int(data[6:7].decode())  # type: ignore
                 chunk = data[7:]
-                
+
                 while iteration > self.sys.iteration:
-                    print("here")
                     time.sleep(0.01)
                 hist = self.sys.state_history[-1]
                 if iteration < self.sys.iteration:
