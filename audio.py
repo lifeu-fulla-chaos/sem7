06213a90a24c11433090ee6e2f1eb58ea37eff07
mbuyt0n <shusrith03@gmail.com>
2025-11-05 00:37:20 +0530

    balls

--- audio.py
+++ audio.py
@@ -91,7 +91,7 @@ class AudioHandler:
                     state = self.sys.state_history[header]  # type: ignore
                 # dec_chunk = decrypt_audio(chunk, header, audio_nonce, auth_tag, state)  # type: ignore
                 audio_array = np.frombuffer(chunk, dtype="<i2")
-                audio_array = np.reshape(chunk, (-1, channels))  # type: ignore
+                audio_array = np.reshape(audio_array, (-1, channels))  # type: ignore
                 print("dec audio", audio_array[:10])
                 stream.write(audio_array)
 
